80feaf3674a30b3a30553abd09edcb7bb5f42bd9
Nestor <nestorpuentesin@gmail.com>
2025-09-19 16:02:29 -0600

    se esta modificando el PDF de la requi diseño y agregar datos faltantes

--- src/requisition/pdf/PdfHeader.tsx
+++ src/requisition/pdf/PdfHeader.tsx
@@ -4,15 +4,18 @@ import { createTw } from "react-pdf-tailwind";
 const tw = createTw({});
 
 const PdfHeader = () => {
-  return (
-    <View style={tw(" flex flex-row")}>
-      <View style={tw(" items-start")}>
-        <Image src={Logo} style={tw("h-40 w-60")} />
+   return (
+      <View style={tw("flex flex-row items-center justify-center mb-4")}>
+         <View style={tw("flex items-center justify-center w-1/3")}>
+            <Image src={Logo} style={tw("h-[50px] object-contain")} />
+         </View>
+         <Text style={tw("flex-1 text-md font-bold text-gray-800 mt-4")}>
+            R. AYUNTAMIENTO DE GOMEZ PALACIO, DGO. 2025-2028
+         </Text>
+         <View style={tw("flex items-center justify-center w-1/3")}>
+            <Image src={Logo} style={tw("h-[50px] object-contain")} />
+         </View>
       </View>
-      <Text style={tw("text-xs font-bold text-gray-800 mt-4")}>
-        R. AYUNTAMIENTO DE GOMEZ PALACIO, DGO 2025-2028
-      </Text>
-    </View>
-  );
+   );
 };
 export default PdfHeader;
--- src/requisition/pdf/Pdfrequisition.tsx
+++ src/requisition/pdf/Pdfrequisition.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useRef, useState } from "react";
 import ModalComponent from "../../components/modal/Modal";
 import {
-  Document,
-  Page,
-  Text,
-  View,
-  PDFViewer,
-  StyleSheet,
-  Image,
+   Document,
+   Page,
+   Text,
+   View,
+   PDFViewer,
+   StyleSheet,
+   Image,
 } from "@react-pdf/renderer";
 import { createTw } from "react-pdf-tailwind";
 import Observable from "../../extras/observable";
@@ -19,67 +19,76 @@ import { PdfRight } from "./PdfRight";
 import PdfFooter from "./PdfFooter";
 import Spinner from "../../loading/Loading";
 type PdfRequisitionType = {
-  open: boolean;
-  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  filePath?: string;
-  children?: React.ReactNode;
+   open: boolean;
+   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+   filePath?: string;
+   children?: React.ReactNode;
 };
 const tw = createTw({});
 
 const PdfRequisition: React.FC<PdfRequisitionType> = ({
-  open,
-  setOpen,
-  filePath,
-  children,
+   open,
+   setOpen,
+   filePath,
+   children,
 }) => {
-  const data = Observable().ObservableGet("PdfRequisicion") as any;
-  const [myData, setData] = useState<Array<Record<string, any>>>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const groupArrays = (
-    array: Array<Record<string, any>>,
-    length: number
-  ): Array<Array<Record<string, any>>> => {
-    const resultado: Array<Array<Record<string, any>>> = [];
-    for (let i = 0; i < array.length; i += length) {
-      resultado.push(array.slice(i, i + length));
-    }
-    setLoading(false);
-    return resultado;
-  };
+   const data = Observable().ObservableGet("PdfRequisicion") as any;
+   const [myData, setData] = useState<Array<Record<string, any>>>([]);
+   const [loading, setLoading] = useState<boolean>(true);
+   const groupArrays = (
+      array: Array<Record<string, any>>,
+      length: number,
+   ): Array<Array<Record<string, any>>> => {
+      const resultado: Array<Array<Record<string, any>>> = [];
+      for (let i = 0; i < array.length; i += length) {
+         resultado.push(array.slice(i, i + length));
+      }
+      setLoading(false);
+      return resultado;
+   };
 
-  useEffect(() => {
-    setData(groupArrays(data?.data?.products, 2));
-  }, []);
+   useEffect(() => {
+      setData(groupArrays(data?.data?.products, 2));
+   }, []);
 
-  return (
-    <ModalComponent title="Requisición" open={open} setOpen={setOpen}>
-      {loading && <Spinner />}
-      <PDFViewer width="100%" height="100%">
-        <Document>
-          {myData.map((item: any) => (
-            <Page size="A4" orientation="landscape">
-              <View style={tw("p-4")}>
-                <PdfHeader />
+   return (
+      <ModalComponent
+         title="Requisición"
+         open={open}
+         setOpen={setOpen}
+         fullScreen>
+         {loading && <Spinner />}
+         <PDFViewer width="100%" height="100%">
+            <Document>
+               {myData.map((item: any) => (
+                  <Page size="A4" orientation="landscape">
+                     <View style={tw("p-4")}>
+                        <PdfHeader />
 
-                <View
-                  style={tw(
-                    "border-2 border-black flex flex-row w-full h-full text-black"
-                  )}
-                >
-                  <PdfLeft products={item} pdfData={data?.data?.pdfData} />
+                        <View
+                           style={tw(
+                              "border-2 border-black flex flex-row w-full h-full text-black",
+                           )}>
+                           <PdfLeft
+                              products={item}
+                              pdfData={data?.data?.pdfData}
+                           />
 
-                  <PdfRight products={item} pdfData={data?.data?.pdfData} />
-                </View>
+                           <PdfRight
+                              products={item}
+                              pdfData={data?.data?.pdfData}
+                           />
+                        </View>
 
-                <PdfFooter data={data} />
-              </View>
-            </Page>
-          ))}
-        </Document>
-      </PDFViewer>
-      {children}
-    </ModalComponent>
-  );
+                        <PdfFooter data={data} />
+                     </View>
+                  </Page>
+               ))}
+            </Document>
+         </PDFViewer>
+         {children}
+      </ModalComponent>
+   );
 };
 
 export default PdfRequisition;
